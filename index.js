eb9c0b6ba83a9b7232651de0a571ec215829ef65
Namhoon Kim <yanhkim@gmail.com>
2015-11-02 22:38:41 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs-extra'));
+var path = require('path');
+
+var getTemplate = function () {
+  return fs.readFileAsync(path.join(__dirname, 'template', 'json.hbs'), 'utf8');
+};
+
+var transform = Promise.method(function (layouts, source, opt, Handlebars) {
+  var template = Handlebars.compile(source);
+  return template({
+    layouts: layouts
+  });
+});
+
+module.exports = {
+  process: function (layouts, opt, Handlebars) {
+    layouts.forEach(function (l) {
+      l.layout.items[l.layout.items.length - 1].last = true;
+      l.sprites[l.sprites.length - 1].last = true;
+    });
+    layouts[layouts.length - 1].last = true;
+    return getTemplate()
+      .then(function (source) {
+        return transform(layouts, source, opt, Handlebars);
+      });
+  },
+  isBeautifyable: function () {
+    return false;
+  },
+  extension: function () {
+    return 'json';
+  }
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# sprity-json
+
+> A json style processor for [sprity](https://npmjs.org/package/sprity)
+
+## Requirements
+
+- [sprity](https://npmjs.org/package/sprity) version >= 1.0
+
+## Install
+
+```sh
+npm install sprity-json
+```
+
+## License
+
+MIT
